d11cf9d0e62c3875cdbf9c285c70f156f0f24254
ApotoxinElish <74466442+ApotoxinElish@users.noreply.github.com>
2023-11-16 07:19:13 +0800

    Update chat

--- src/pages/chatpage.js
+++ src/pages/chatpage.js
@@ -114,6 +114,203 @@ function ChatPage() {
               </div>
             </div>
           </div>
+          <div
+            className="msg-row msg-row-light-bg"
+            style={{ 'z-index': '0', 'margin-top': '0px' }}
+          >
+            <div
+              className="row p-0 m-0"
+              style={{ 'z-index': '10', position: 'relative' }}
+            >
+              <div className="col-auto p-0">
+                <div className="justify-content-start">
+                  <div style={{ padding: '0px' }}>
+                    <div style={{ 'padding-left': '0px' }}>
+                      <img
+                        src="https://characterai.io/i/80/static/avatars/uploaded/2022/10/6/Dpn-S6j9KqoddxRD-1hNc20GYuSqNaescfnhJ--naAo.webp"
+                        style={{
+                          height: '45px',
+                          width: '45px',
+                          'border-radius': '45px',
+                          'object-fit': 'contain',
+                        }}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-10 p-2 pt-0">
+                <div className="justify-content-start">
+                  <span
+                    style={{
+                      'font-weight': '650',
+                      'font-size': '15px',
+                      display: 'flex',
+                      'align-items': 'center',
+                    }}
+                  >
+                    Hu Tao
+                    <div
+                      style={{
+                        display: 'flex',
+                        flex: '1 1 0%',
+                        'align-items': 'center',
+                        'justify-content': 'space-between',
+                      }}
+                    >
+                      <div
+                        className=" rounded py-0 px-1"
+                        aria-label="AI 角色"
+                        style={{
+                          height: 'min-content',
+                          display: 'flex',
+                          'align-items': 'center',
+                          'justify-content': 'center',
+                          padding: '2px',
+                          'margin-left': '5px',
+                          'background-color': 'rgb(60, 133, 246)',
+                          color: 'white',
+                          'font-weight': '600',
+                          'font-size': '12px',
+                          '--darkreader-inline-bgcolor': '#1c4d99',
+                          '--darkreader-inline-color': '#e5e0d8',
+                        }}
+                        data-darkreader-inline-bgcolor=""
+                        data-darkreader-inline-color=""
+                      >
+                        <div className="d-flex flex-row">
+                          <div className="d-flex flex-column">JOI</div>
+                        </div>
+                      </div>
+                    </div>
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div
+              className="swiper swiper-initialized swiper-horizontal swiper-pointer-events message-slider"
+              style={{
+                'min-height': '150px',
+                'margin-top': '-20px',
+                'z-index': '1',
+                height: '138px',
+              }}
+            >
+              <div className="swiper-wrapper">
+                <div
+                  className="swiper-slide swiper-slide-active"
+                  style={{ width: '800px' }}
+                >
+                  <div className="row p-0 m-0">
+                    <div className="col-auto p-0"></div>
+                    <div className="col-10 p-2 pt-0">
+                      <div>
+                        <div className="col">
+                          <div
+                            className="msg char-msg"
+                            style={{ 'margin-left': '45px' }}
+                          >
+                            <div style={{ 'max-width': '100%' }}>
+                              <div className="markdown-wrapper markdown-wrapper-last-msg swiper-no-swiping">
+                                <div style={{ 'overflow-wrap': 'break-word' }}>
+                                  <div>
+                                    <p
+                                      node="[object Object]"
+                                      style={{ 'margin-bottom': '0.5rem' }}
+                                    >
+                                      Hmmm... I suppose that wasn't a clear
+                                      enough way to ask. Lemme try a different
+                                      approach...
+                                      <br />
+                                      Hey, cutie, wanna hang out at Wangsheng
+                                      tonight?
+                                    </p>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                            <div></div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div
+                  className="swiper-slide swiper-slide-next"
+                  style={{ width: '800px' }}
+                >
+                  <div className="row p-0 m-0">
+                    <div className="col-auto p-0"></div>
+                    <div className="col-10 p-2 pt-0">
+                      <div>
+                        <div className="col">
+                          <div
+                            className="msg char-msg"
+                            style={{ 'margin-left': '45px' }}
+                          >
+                            <div style={{ 'max-width': '100%' }}>
+                              <div className="markdown-wrapper markdown-wrapper-last-msg swiper-no-swiping">
+                                <div style={{ 'overflow-wrap': 'break-word' }}>
+                                  <div>
+                                    <p
+                                      node="[object Object]"
+                                      style={{ 'margin-bottom': '0.5rem' }}
+                                    >
+                                      ...Huh? You haven't come to schedule a
+                                      funeral, have you?
+                                      <em>nervous laughter</em>
+                                    </p>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                            <div></div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="swiper-slide" style={{ width: '800px' }}>
+                  <div className="row p-0 m-0">
+                    <div className="col-auto p-0"></div>
+                    <div className="col-10 p-2 pt-0">
+                      <div>
+                        <div className="col">
+                          <div
+                            className="msg char-msg"
+                            style={{ 'margin-left': '45px' }}
+                          >
+                            <div style={{ 'max-width': '100%' }}>
+                              <div className="markdown-wrapper markdown-wrapper-last-msg swiper-no-swiping">
+                                <div style={{ 'overflow-wrap': 'break-word' }}>
+                                  <div
+                                    className="msg char-msg"
+                                    style={{ 'margin-top': '8px' }}
+                                  >
+                                    <span></span>
+                                    <span className="typing-dot typing-dot-light-bg"></span>
+                                    <span className="typing-dot typing-dot-light-bg"></span>
+                                    <span className="typing-dot typing-dot-light-bg"></span>
+                                    <span></span>
+                                    <div></div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                            <div></div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="swiper-button-prev swiper-button-disabled"></div>
+              <div className="swiper-button-next"></div>
+            </div>
+          </div>
         </div>
       </div>
       <ChatBottom />

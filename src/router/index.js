cac58b82e275f19e2c45f56bf32eb719002eb566
ApotoxinElish <74466442+ApotoxinElish@users.noreply.github.com>
2023-11-24 15:31:07 +0800

    config router

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+// Route configuration
+
+import Layout from '../pages/Layout'
+import Login from '../pages/Login'
+
+import { createBrowserRouter } from 'react-router-dom'
+
+// Configuring a route instance
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+])
+
+export default router

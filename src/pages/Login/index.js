14bf8f561cde4a1069f5ec28dcae9a3307d2f6f3
ApotoxinElish <74466442+ApotoxinElish@users.noreply.github.com>
2023-11-25 02:35:32 +0800

    static structure

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,5 +1,32 @@
+import './index.scss'
+import { Card, Form, Input, Button } from 'antd'
+// import logo from '@/assets/logo.png'
+
 const Login = () => {
-  return <div>this is Login</div>
+  return (
+    <div className="login">
+      <Card className="login-container">
+        {/* <img className="login-logo" src={logo} alt="" /> */}
+        {/* Entry form */}
+        <Form>
+          <Form.Item>
+            <Input size="large" placeholder="Please enter your phone number" />
+          </Form.Item>
+          <Form.Item>
+            <Input
+              size="large"
+              placeholder="Please enter the verification code"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              Log in
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  )
 }
 
 export default Login

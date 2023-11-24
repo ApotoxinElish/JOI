f59ec7d0815c331c1b7ecbe5021855a408ea68e6
ApotoxinElish <74466442+ApotoxinElish@users.noreply.github.com>
2023-11-25 02:01:54 +0800

    import craco and alias

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 // Route configuration
 
-import Layout from '../pages/Layout'
-import Login from '../pages/Login'
+import Layout from '@/pages/Layout' // src/pages/layout
+import Login from '@/pages/Login'
 
 import { createBrowserRouter } from 'react-router-dom'
 
